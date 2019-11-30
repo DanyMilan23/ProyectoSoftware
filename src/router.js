4bef33413890191f185933d95f533669f534b97a
dany_milan <esteban239222@gmail.com>
2019-11-30 00:30:15 -0400

    Router configurado

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+  } from "react-router-dom";
+
+
+class Router extends React.Component{
+    render(){
+        return(
+            <BrowserRouter>
+                
+                <Switch>
+                    
+                </Switch>
+            </BrowserRouter>
+             
+        );
+    }
+}
+export default Router;
