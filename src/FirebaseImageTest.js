02b82087c11512f2a6c25942b4f8eb6b23bcd18b
OscarIS <oscaris087@gmail.com>
2019-11-30 18:00:00 -0400

    FirebaseImageTest Added

---
+++ src/FirebaseImageTest.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+class FirebaseImageTest extends React.Component{
+    render(){
+        return(
+            <img src="https://firebasestorage.googleapis.com/v0/b/softwarecommerce23.appspot.com/o/imagen%2Ftecnologia.jpg?alt=media&token=bc14d7a3-86a2-4058-86fa-74635d25d720"/>
+
+        );
+    }
+}
+export default FirebaseImageTest;
